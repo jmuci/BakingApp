1908eeeda57f3f5b7f60d173882792c4fbfe61aa
Jose Mucientes <jmucientes@spotify.com>
2018-12-16 12:13:34 +0100

    Add Json Model classes and Gson dependency

---
+++ main/src/main/java/com/jmucientes/udacity/bakingapp/main/model/Ingredient.java
@@ -0,0 +1,80 @@
+
+package com.jmucientes.udacity.bakingapp.main.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient implements Parcelable {
+
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+    public final static Creator<Ingredient> CREATOR = new Creator<Ingredient>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+
+        public Ingredient[] newArray(int size) {
+            return (new Ingredient[size]);
+        }
+
+    };
+
+    protected Ingredient(Parcel in) {
+        this.quantity = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.measure = ((String) in.readValue((String.class.getClassLoader())));
+        this.ingredient = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    public Ingredient() {
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(quantity);
+        dest.writeValue(measure);
+        dest.writeValue(ingredient);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}

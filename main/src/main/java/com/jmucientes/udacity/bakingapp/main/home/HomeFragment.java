92fd7d2fce6618690a0e2052da78c8b41be93546
Jose Mucientes <jmucientes@spotify.com>
2018-12-26 11:49:56 +0100

    Implement pull to refresh

--- main/src/main/java/com/jmucientes/udacity/bakingapp/main/home/HomeFragment.java
+++ main/src/main/java/com/jmucientes/udacity/bakingapp/main/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -25,6 +26,9 @@ public class HomeFragment extends DaggerFragment {
 
     private RecipeViewModel mViewModel;
     private RecyclerView mRecipeCardsRV;
+    private SwipeRefreshLayout mSwipeRefresh;
+
+
     @Inject
     RecipeAdapter mRecipeAdapter;
     @Inject
@@ -42,6 +46,11 @@ public class HomeFragment extends DaggerFragment {
         View view =  inflater.inflate(R.layout.home_fragment, container, false);
         mRecipeCardsRV = view.findViewById(R.id.recipes_recycler_view);
         mProgressBar = view.findViewById(R.id.progress_bar);
+        mSwipeRefresh = view.findViewById(R.id.main_swipe_container);
+
+        mSwipeRefresh.setOnRefreshListener(() -> {
+            refreshDataSetFromNetwork();
+        });
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -55,10 +64,25 @@ public class HomeFragment extends DaggerFragment {
         return view;
     }
 
+    private void refreshDataSetFromNetwork() {
+        mViewModel.refreshData().observe(this, recipeList -> {
+             if (recipeList != null && recipeList.size() > 0) {
+                 mRecipeAdapter.updateDataSet(recipeList);
+             } else {
+                 Toast.makeText(this.getContext(), "Failed to contact server.", Toast.LENGTH_SHORT).show();
+             }
+             mSwipeRefresh.setRefreshing(false);
+         });
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this, mViewModelFactory).get(RecipeViewModel.class);
+        requestDataSetLoadAndObserveChanges();
+    }
+
+    private void requestDataSetLoadAndObserveChanges() {
         mViewModel.getRecipeList().observe(this, recipeList -> {
             mProgressBar.setVisibility(View.GONE);
             if (recipeList != null && recipeList.size() > 0) {

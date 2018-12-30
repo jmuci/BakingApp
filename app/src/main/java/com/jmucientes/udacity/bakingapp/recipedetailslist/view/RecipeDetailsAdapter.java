6b61886a548c082719d2fd93b971aa3e12c5c1f9
Jose Mucientes <jmucientes@spotify.com>
2018-12-30 23:44:07 +0100

    Style the video player fragment

--- app/src/main/java/com/jmucientes/udacity/bakingapp/recipedetailslist/view/RecipeDetailsAdapter.java
+++ app/src/main/java/com/jmucientes/udacity/bakingapp/recipedetailslist/view/RecipeDetailsAdapter.java
@@ -1,15 +1,21 @@
 package com.jmucientes.udacity.bakingapp.recipedetailslist.view;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.jmucientes.udacity.bakingapp.MainActivity;
 import com.jmucientes.udacity.bakingapp.R;
 import com.jmucientes.udacity.bakingapp.model.Step;
+import com.jmucientes.udacity.bakingapp.stepdetail.StepDetailFragment;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +24,7 @@ import javax.inject.Inject;
 public class RecipeDetailsAdapter extends RecyclerView.Adapter<RecipeDetailsAdapter.DetailViewHolder> {
 
     private List<Step> mStepList;
+    private WeakReference<Context> mContextWeakReference;
 
     @Inject
     public RecipeDetailsAdapter() {
@@ -33,13 +40,13 @@ public class RecipeDetailsAdapter extends RecyclerView.Adapter<RecipeDetailsAdap
             super(itemView);
             mStepNumber = itemView.findViewById(R.id.step_number);
             mDetailsShortDescriptionTV = itemView.findViewById(R.id.short_desc);
-
         }
     }
 
     @NonNull
     @Override
     public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        mContextWeakReference = new WeakReference<>(parent.getContext());
         ConstraintLayout stepTV = (ConstraintLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.step_list_item, parent, false);
         return new DetailViewHolder(stepTV);
     }
@@ -48,6 +55,22 @@ public class RecipeDetailsAdapter extends RecyclerView.Adapter<RecipeDetailsAdap
     public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int position) {
         detailViewHolder.mStepNumber.setText(String.valueOf(position + 1));
         detailViewHolder.mDetailsShortDescriptionTV.setText(mStepList.get(position).getShortDescription());
+
+        detailViewHolder.mDetailsShortDescriptionTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToStepDetails(mStepList.get(position));
+            }
+        });
+    }
+
+    private void navigateToStepDetails(Step step) {
+        StepDetailFragment stepDetailFragment = new StepDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(StepDetailFragment.ARG_STEP, step);
+        stepDetailFragment.setArguments(args);
+
+        ((MainActivity) mContextWeakReference.get()).navigateToFragment(stepDetailFragment);
     }
 
     @Override

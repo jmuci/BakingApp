89002d6cc6e01b2db7a8047e19e2006654bc4e0d
Jose Mucientes <jmucientes@spotify.com>
2019-01-02 20:33:15 +0100

    Add exo player

--- app/src/main/java/com/jmucientes/udacity/bakingapp/stepdetail/StepDetailFragment.java
+++ app/src/main/java/com/jmucientes/udacity/bakingapp/stepdetail/StepDetailFragment.java
@@ -1,5 +1,6 @@
 package com.jmucientes.udacity.bakingapp.stepdetail;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.jmucientes.udacity.bakingapp.R;
 import com.jmucientes.udacity.bakingapp.model.Step;
 
@@ -17,9 +26,14 @@ import dagger.android.support.DaggerFragment;
 
 public class StepDetailFragment extends DaggerFragment {
 
-    public static final String ARG_STEP = "step_parcelable";;
+    public static final String ARG_STEP = "step_parcelable";
+    ;
 
     private TextView mStepDescription;
+    private SimpleExoPlayerView mPlayerView;
+
+    @Inject
+    SimpleExoPlayer mPlayer;
 
     @Inject
     public StepDetailFragment() {
@@ -30,18 +44,48 @@ public class StepDetailFragment extends DaggerFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.step_detail_fragment, container, false);
         mStepDescription = view.findViewById(R.id.step_description);
+        mPlayerView = view.findViewById(R.id.player_view);
 
         Bundle extras = getArguments();
         if (extras != null) {
             Step step = extras.getParcelable(ARG_STEP);
-            if (step !=null )
+            if (step != null) {
                 mStepDescription.setText(step.getDescription());
+                initializePlayer(Uri.parse(step.getVideoURL()));
+            }
         }
+
         return view;
+    }
+
+    private void initializePlayer(Uri mp4VideoUri) {
+        if (mPlayer != null && mp4VideoUri != null) {
+            mPlayerView.setPlayer(mPlayer);
+            // Produces DataSource instances through which media data is loaded.
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getActivity(),
+                    Util.getUserAgent(getActivity(), "BakingApp"));
+            // This is the MediaSource representing the media to be played.
+            MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri, dataSourceFactory, new DefaultExtractorsFactory(), null, null);
+            // Prepare the player with the source.
+            mPlayer.prepare(videoSource);
+            mPlayer.setPlayWhenReady(true);
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        mPlayer.stop();
+        mPlayer.release();
+        mPlayer = null;
     }
 }
--- app/src/main/java/com/jmucientes/udacity/bakingapp/stepdetail/di/StepDetailModule.java
+++ app/src/main/java/com/jmucientes/udacity/bakingapp/stepdetail/di/StepDetailModule.java
@@ -1,9 +1,22 @@
 package com.jmucientes.udacity.bakingapp.stepdetail.di;
 
+import android.content.Context;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.jmucientes.udacity.bakingapp.MainActivity;
+import com.jmucientes.udacity.bakingapp.di.scopes.ActivityScope;
 import com.jmucientes.udacity.bakingapp.di.scopes.FragmentScoped;
 import com.jmucientes.udacity.bakingapp.stepdetail.StepDetailFragment;
 
 import dagger.Module;
+import dagger.Provides;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
@@ -12,4 +25,23 @@ public abstract class StepDetailModule {
     @FragmentScoped
     @ContributesAndroidInjector
     abstract StepDetailFragment stepDetailFragment();
+
+
+    @ActivityScope
+    @Provides
+    static SimpleExoPlayer simpleExoPlayerProvider(MainActivity mainActivity, TrackSelector trackSelector, LoadControl loadControl) {
+        return ExoPlayerFactory.newSimpleInstance(mainActivity, trackSelector, loadControl);
+    }
+
+    @ActivityScope
+    @Provides
+    static TrackSelector trackSelectorProvider() {
+        return new DefaultTrackSelector();
+    }
+
+    @ActivityScope
+    @Provides
+    static LoadControl loadControlProvider() {
+        return new DefaultLoadControl();
+    }
 }

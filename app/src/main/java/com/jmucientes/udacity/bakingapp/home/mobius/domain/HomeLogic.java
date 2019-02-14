dd09d1503951fd1fa4c528c23dda06ae50ef6dab
Jose Mucientes <jmucientes@spotify.com>
2019-02-14 07:51:10 +0100

    Add a new unit test and fix existing

--- app/src/main/java/com/jmucientes/udacity/bakingapp/home/mobius/domain/HomeLogic.java
+++ app/src/main/java/com/jmucientes/udacity/bakingapp/home/mobius/domain/HomeLogic.java
@@ -1,7 +1,6 @@
 package com.jmucientes.udacity.bakingapp.home.mobius.domain;
 
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.spotify.mobius.First;
 import com.spotify.mobius.Next;
@@ -13,8 +12,6 @@ import static com.spotify.mobius.Effects.effects;
 import static com.spotify.mobius.First.first;
 
 public final class HomeLogic {
-
-    private static final String TAG = HomeLogic.class.getName();
 
     private HomeLogic() {}
 
@@ -35,20 +32,16 @@ public final class HomeLogic {
 
     private static Next<HomeModel, HomeEffect> onRecipeCardClicked(HomeModel model, HomeEvent.RecipeCardClicked recipeCardClickedEvent) {
         if (model.recipes() != null && !model.recipes().isEmpty()) {
-            Log.d(TAG, "update().onRecipeCardClicked() -> Next.next(effect(navToRecipe)). Recipe: " + recipeCardClickedEvent.recipe());
             return Next.dispatch(effects(navigateToRecipeDetailsList(recipeCardClickedEvent.recipe())));
         } else {
-            Log.d(TAG, "update().onRecipeCardClicked() -> Next.noChange(). Recipe: " + recipeCardClickedEvent.recipe());
             return Next.noChange();
         }
     }
 
     private static Next<HomeModel,HomeEffect> onRecipesLoaded(HomeModel model, HomeEvent.RecipesLoaded recipesLoadedEvent) {
         if (recipesLoadedEvent.recipes().isEmpty() || recipesLoadedEvent.recipes().equals(model.recipes())) {
-            Log.d(TAG, "update().onRecipesLoaded() -> Next.noChange(). Recipes: " + recipesLoadedEvent.recipes());
             return Next.noChange();
         }
-        Log.d(TAG, "update().onRecipesLoaded() -> Next.next(model.with). Recipes: " + recipesLoadedEvent.recipes());
         return Next.next(model.withRecipes(recipesLoadedEvent.recipes()));
     }
 
